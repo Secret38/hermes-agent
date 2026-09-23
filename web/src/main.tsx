8ec6c51f3cc4f6568c7d30b37a4751c5ea2b8021
SECRET EMKO <emkophotography@gmail.com>
2026-09-23 23:13:58 +0200

    chore(a11y): integrate accessibility hardening: main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,6 +7,7 @@ import { I18nProvider } from "./i18n";
 import { exposePluginSDK } from "./plugins";
 import { ThemeProvider } from "./themes";
 import { HERMES_BASE_PATH } from "./lib/api";
+import { ModalAccessibilityGuard } from "./components/ModalAccessibilityGuard";
 
 // Expose the plugin SDK before rendering so plugins loaded via <script>
 // can access React, components, etc. immediately.
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")!).render(
     <I18nProvider>
       <ThemeProvider>
         <SystemActionsProvider>
+          <ModalAccessibilityGuard />
           <App />
         </SystemActionsProvider>
       </ThemeProvider>
